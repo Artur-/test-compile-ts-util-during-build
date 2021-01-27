9b3556dcdfc22e0273baa68d990b7fdd48ba1b89
Artur Signell <artur@vaadin.com>
2021-01-27 15:00:55 +0200

    Generated

---
+++ src/main/java/com/example/application/Application.java
@@ -0,0 +1,28 @@
+package com.example.application;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.vaadin.artur.helpers.LaunchUtil;
+import com.vaadin.flow.theme.Theme;
+
+/**
+ * The entry point of the Spring Boot application.
+ *
+ * Use the * and some desktop browsers.
+ *
+ */
+@SpringBootApplication
+@Theme(value = "compile-ts-util-during-build")
+@PWA(name = "compile-ts-util-during-build", shortName = "compile-ts-util-during-build", offlineResources = {
+        "images/logo.png"})
+public class Application extends SpringBootServletInitializer implements AppShellConfigurator {
+
+    public static void main(String[] args) {
+        LaunchUtil.launchBrowserInDevelopmentMode(SpringApplication.run(Application.class, args));
+    }
+
+}
